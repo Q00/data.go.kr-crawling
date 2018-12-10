b2ccfd45250b97dc3d654c5de235da19c0047e9c
Q00 <jqyu.lee@gmail.com>
2018-12-10 10:26:50 +0900

    add remark and fix head

--- go_data_crwaler.py
+++ go_data_crwaler.py
@@ -21,9 +21,12 @@ baseUrl = 'http://apis.data.go.kr/1470000/DURPrdlstInfoService/'
 urlList = 'getDurPrdlstInfoList','getSeobangjeongPartitnAtentInfoList','getEfcyDplctInfoList','getOdsnAtentInfoList','getMdctnPdAtentInfoList','getCpctyAtentInfoList','getPwnmTabooInfoList','getSpcifyAgrdeTabooInfoList','getUsjntTabooInfoList'
 
 for addUrl in urlList:
+    
+    main_row = 1 
+    cell_num = 1
     #a~z loop
     for letter in range(ord('a'), ord('z')+1):
-        params = {'itemName': chr(letter), 'Servicekey': key, 'numOfRows':1}
+        params = {'itemName': chr(letter), 'Servicekey': key, 'numOfRows':100}
         params_str = "&".join("%s=%s" % (k,v) for k,v in params.items())
         requesturl = baseUrl + addUrl
         #print(requesturl)
@@ -35,38 +38,59 @@ for addUrl in urlList:
             print('request error')
         try:
             soup = BS(getdata.text, 'lxml-xml') 
-            #print(soup.prettify)
-            okflag = soup.find('resultCode')
-            #validation check
-            if okflag.text != '00':
-                print(okflag.text)
-                raise ValueError('okcode is not 00')    
-            else:
-                #check page
-                totalCount = int(soup.find('totalCount').text)
-                page = int(totalCount/100) + 1
-                for item in soup.find_all('item'):
-                    main_row = 1 
-                    cell_num = 1
-                    for child in item.children:
-                        if child != '\n':
-                            print(child.name)
-                            #print(child.contents)
-                            #print(column.getDurPrdlstInfoList[child.name])
-                            #엑셀 헤드 부분
-                            if main_row == 1:
-                                evalue = column.getDurPrdlstInfoList[child.name]
-                                print('print head')
-                            else:
-                                evalue = child.contents
-                                print('print body',main_row)
+            
+            #check page
+            totalCount = int(soup.find('totalCount').text)
+            print('총 개수: ',totalCount)
+            page = int(totalCount/100) + 1
+            print('총 페이지: ',page) 
+            #cell number parameter
+            for i in range(page):
+                params_str2 = params_str
+                params_str2+= '&pageNo='+str(i+1)
+                #request again
+                print(str(i+1)+'페이지 가져오는중')
+                getdata = requests.get(requesturl, params=params_str2)
+                soup = BS(getdata.text,'lxml-xml') 
+                #print(soup.prettify)
 
-                            ws.cell(main_row,cell_num).value = evalue 
-                            cell_num+=1
-                    main_row+=1
+                #validation check
+                okflag = soup.find('resultCode')
+                
+                if okflag.text != '00':
+                    print("okflag: ",okflag.text)
+                    
+                    raise ValueError('okcode is not 00')    
+                else:
+                    #검색이 잘 되었을때
+                    for item in soup.find_all('item'):
+                        #헤드 부분
+                        if main_row ==1: 
+                            for child in item.children:
+                                if child != '\n':
+                                    evalue = column.getDurPrdlstInfoList[child.name]
+                                    ws.cell(main_row,cell_num).value = evalue 
+                                    cell_num+=1
+                            main_row+=1
+
+                        #cell number 초기화
+                        cell_num =1
+
+                        for child in item.children:
+                            if child != '\n':
+                                #print(child.contents)
+                                #print(column.getDurPrdlstInfoList[child.name])
+                                #엑셀 헤드 부분
+                                evalue = str(child.text)
+
+                                ws.cell(main_row,cell_num).value = evalue 
+                                cell_num+=1
+                        main_row+=1
+                print(str(i+1)+'페이지 완료')
         except ValueError as error:
             print(error) 
         break
     break
 
+wb.save('dur.xlsx')
 
