7418a1921708faa9b886bc924284cb48b25e2b06
Q00 <jqyu.lee@gmail.com>
2018-12-13 15:20:06 +0900

    seperate function

--- async_data_crawler.py
+++ async_data_crawler.py
@@ -27,11 +27,9 @@ baseUrl = 'http://apis.data.go.kr/1470000/DURPrdlstInfoService/'
 urlList = 'getDurPrdlstInfoList','getSeobangjeongPartitnAtentInfoList','getEfcyDplctInfoList','getOdsnAtentInfoList','getMdctnPdAtentInfoList','getCpctyAtentInfoList','getPwnmTabooInfoList','getSpcifyAgrdeTabooInfoList','getUsjntTabooInfoList'
 
 main_row=0
-
 def makeCSV(item):
     global main_row
     main_row+=1
-    print(main_row)
     #global main_row
     #item을 하나씩 받아 csv파일로 무조건 add
     #if main_row ==1: 
@@ -50,11 +48,9 @@ def makeCSV(item):
 
 def getData():
     #가지고 있는 url만큼만 loop
-    while True:
-        try:
+    while not queue.empty():
             #저장되어있는 link를 queue에서 가져옴
             #pool의 worker들이 link로 request 동기보다 n배 빠름
-            if not queue.empty() :
                 link = queue.get(timeout=0)
                 if link != "":
                     getdata = requests.get(link)
@@ -62,73 +58,82 @@ def getData():
                     #print(soup.prettify)
                     #validation check
                     okflag = soup.find('resultCode')
-                    print(okflag) 
-                    if okflag.text != '00':
-                        print("okflag: ",okflag.text)
-                        
-                        raise ValueError('okcode is not 00')    
-                    else:
-                        #검색잘되면 엑셀 파싱
-                        #pool map method vs pool map_async
-                        #어떤것이 더 효율이 좋을지 결정필요
-                        #pool 워커안에 pool넣을 수 있는지 확인 필요
-                        pool_excel.map(makeCSV,soup.find_all('item'))
-
+                    try:
+                        if okflag.text != '00':
+                            print("okflag: ",okflag.text)
+                            
+                            raise ValueError('okcode is not 00')    
+                        else:
+                            #검색잘되면 엑셀 파싱
+                            #pool map method vs pool map_async
+                            #어떤것이 더 효율이 좋을지 결정필요
+                            pool_excel.map(makeCSV,soup.find_all('item'))
+                    
+                    except:
+                        #print(soup.prettify)
+                        print(link)
 
             #param이 다 추가된 link에 대해서 queue에 저장 getData()의 3번째줄에서 저장되어있는 link가져감
-            
-            for addUrl in urlList:
-                for letter in range(ord('a'), ord('z')+1):
-                    params = {'itemname': chr(letter), 'servicekey': key, 'numofrows':100}
-                    if addUrl != 'getdurprdlstinfolist':
-                        params.update({'typename' : column.typeName[addUrl]})
-                    params_str = "&".join("%s=%s" % (k,v) for k,v in params.items())
-                    requestUrl = baseUrl + addUrl
-                    #print(requesturl)
-                    try:
-                        #request data
-                        print('데이터 가져오는중 검색한 알파벳 :', chr(letter))
-                        getdata = requests.get(requestUrl, params=params_str)
-                    except:
-                        print('request error')
-                    try:
-                        soup = BS(getdata.text, 'lxml-xml') 
-                    
-                        #print(soup.prettify) 
-                        #check page
-                        try:
-                            totalcount = int(soup.find('totalCount').text)
-                        except:
-                            print('검색결과없음')
-                            totalcount=0
-                        #print('총 개수: ',totalcount)
-                        if totalcount!=0:
-                            page = int(totalcount/100) + 1
-                            #cell number parameter
-                            #print('페이지 : ',page)
-                            for i in range(page):
-                                params_str2 = params_str
-                                params_str2+= '&pageno='+str(i+1)
-                                #request again
-                                queue.put(requestUrl+'?'+params_str2)
-                                #print(requestUrl+params_str2)
-             
-                    except:
-                        print('crwaling error : ',sys.exc_info()[1])
-                break
-        except gevent.queue.Empty:
-            break
     print('stop crwaling')
+
+
+           
+def getLink():
+    print('getlink') 
+    #param이 다 추가된 link에 대해서 queue에 저장 getData()의 3번째줄에서 저장되어있는 link가져감
+    
+    for addUrl in urlList:
+        for letter in range(ord('a'), ord('z')+1):
+            params = {'itemname': chr(letter), 'servicekey': key, 'numofrows':100}
+            if addUrl != 'getdurprdlstinfolist':
+                params.update({'typename' : column.typeName[addUrl]})
+            params_str = "&".join("%s=%s" % (k,v) for k,v in params.items())
+            requestUrl = baseUrl + addUrl
+            #print(requesturl)
+            try:
+                #request data
+                print('데이터 가져오는중 검색한 알파벳 :', chr(letter))
+                getdata = requests.get(requestUrl, params=params_str)
+            except:
+                print('request error')
+            try:
+                soup = BS(getdata.text, 'lxml-xml') 
+            
+                #print(soup.prettify) 
+                #check page
+                try:
+                    totalcount = int(soup.find('totalCount').text)
+                except:
+                    print('검색결과없음')
+                    totalcount=0
+                #print('총 개수: ',totalcount)
+                if totalcount!=0:
+                    page = int(totalcount/100) + 1
+                    #cell number parameter
+                    #print('페이지 : ',page)
+                    for i in range(page):
+                        params_str2 = params_str
+                        params_str2+= '&pageno='+str(i+1)
+                        #request again
+                        queue.put(requestUrl+'?'+params_str2)
+                        print(requestUrl+params_str2)
+     
+            except:
+                print('crwaling error : ',sys.exc_info()[1])
+            break
+        break
 
 #queue init
 queue.put("")
-pool.spawn(getData)
+pool.spawn(getLink).join()
 
 #give worker pool
 print('start crwaling')
 #while not pool.free_count() == 15:
-while True:
+while not queue.empty() :
     gevent.sleep(0.1)
+    print(queue.qsize())
+    print(queue.empty())
     for x in range(0, min(queue.qsize(), pool.free_count())):
         pool.spawn(getData)
 
