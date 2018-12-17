ee343afa9ea41aab511f710df6476e6c1e1740c7
Q00 <jqyu.lee@gmail.com>
2018-12-17 15:14:56 +0900

    edit timegap and err handling

--- async_data_crawler.py
+++ async_data_crawler.py
@@ -20,13 +20,14 @@ queue = gevent.queue.Queue()
 
 #api key loading
 key = config.go_data_api_key
-
+error_count =0
+main_row=0
 #url list 이후에 좀더 일반화해서 편히할 예정
 #해당 부분 csv파일을 만들어 넣었을때 자동으로 파싱할 수 있게  만들 예정
 baseUrl = 'http://apis.data.go.kr/1470000/DURPrdlstInfoService/'
 
 def makeCSV(item):
-    main_row=0
+    global main_row
     main_row+=1
     #global main_row
     #item을 하나씩 받아 csv파일로 무조건 add
@@ -46,9 +47,8 @@ def makeCSV(item):
 
 def getData():
     #가지고 있는 url만큼만 loop
-     
+    global error_count 
     error_log = open('./err.txt',mode='a')
-    error_count = 0
     while not queue.empty():
             #저장되어있는 link를 queue에서 가져옴
             #pool의 worker들이 link로 request 동기보다 n배 빠름
@@ -57,7 +57,6 @@ def getData():
                     gevent.sleep(0.3)
                     getdata = requests.get(link)
                     soup = BS(getdata.text,'lxml-xml') 
-                    #print(soup.prettify)
                     #validation check
                     okflag = soup.find('resultCode')
                     try:
@@ -69,15 +68,18 @@ def getData():
                             #검색잘되면 엑셀 파싱
                             #pool map method vs pool map_async
                             #어떤것이 더 효율이 좋을지 결정필요
+                            print(len(soup.find_all('item')))
                             pool_excel.map(makeCSV,soup.find_all('item'))
                     
                     except:
                         error_log.write(link+'\n')
                         error_log.write('==================================\n')
                         error_count+=1
-                        error_log.write(error_count+'\n')
+                        error_log.write(str(error_count)+'\n')
+                        queue.put(link)
 
     print('stop crwaling')
+    print(main_row)
     error_log.close()
 
          
@@ -129,16 +131,14 @@ def getLink():
 def init():
 
     #queue init
-    main.queue.put("")
-    main.pool.spawn(getLink).join()
+    #main.queue.put("")
+    #main.pool.spawn(getLink).join()
 
     #give worker pool
     print('start crwaling')
     #while not pool.free_count() == 15:
     while not queue.empty() :
-        gevent.sleep(0.5)
-        print(queue.qsize())
-        print(queue.empty())
+        gevent.sleep(0.8)
         for x in range(0, min(queue.qsize(), pool.free_count())):
             pool.spawn(getData)
 
