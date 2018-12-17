ea271ce3469e95e528ef315b8ad97e35b2275465
Q00 <jqyu.lee@gmail.com>
2018-12-17 18:33:57 +0900

    크롤링 잠시 변경

--- url.py
+++ url.py
@@ -12,8 +12,8 @@ def main():
 
 def getData(inputString):
     #param data oprtinSeqNo 와 dur 유형 매칭 필요
-    #paramData = {"publicDataDetailPk":"uddi:fa2ebf2c-6453-4f9d-bd89-ef8e3c77e19f","paramtrSe":"2","oprtinSeqNo":16752}
-    paramData = {"publicDataDetailPk":"uddi:9a60503c-b31c-4879-9028-a4250f0f6998","paramtrSe":"2","oprtinSeqNo":15920}
+    paramData = {"publicDataDetailPk":"uddi:fa2ebf2c-6453-4f9d-bd89-ef8e3c77e19f","paramtrSe":"2","oprtinSeqNo":16754}
+   # paramData = {"publicDataDetailPk":"uddi:9a60503c-b31c-4879-9028-a4250f0f6998","paramtrSe":"2","oprtinSeqNo":15920}
     headers = {'Accept':'application/json, text/plain, */*'}
     getdata = requests.post(inputString,headers=headers, data=json.dumps(paramData))
     json_data = json.loads(getdata.text) 
@@ -22,8 +22,8 @@ def getData(inputString):
         import column
         #column으로 가져와서 할 수 있는 api와 크롤링을 할 수 없는 api(문서가없음)로 인하여
         #직접 하드코딩 해야할때도 있음
-        columnpy.write(column.typeName_reverse[result[8]['paramtrBassValue']]+'=')
-        #columnpy.write(column.typeName_reverse['dictionary name']]+'=')
+        #columnpy.write(column.typeName_reverse[result[8]['paramtrBassValue']]+'=')
+        columnpy.write(column.typeName_reverse['getdur']]+'=')
         excel_head = {}
         for columnIndex in range(6,len(result)):
             if columnIndex ==8:
