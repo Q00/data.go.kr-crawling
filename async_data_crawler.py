c3fc4f4d2dac0ffe4e1017a10e744ccf574b21cf
Q00 <jqyu.lee@gmail.com>
2019-01-02 11:18:31 +0900

    map argument error fix #11

--- async_data_crawler.py
+++ async_data_crawler.py
@@ -21,7 +21,6 @@ queue = gevent.queue.Queue()
 #api key loading
 key = config.go_data_api_key
 error_count =0
-main_row=0
 
 #csv list
 excel_file_name=''
@@ -30,14 +29,15 @@ excel_file_name=''
 file_name = sys.argv[0][:-3]
 
 df_xml = None
+
+def multi_wrapper(args):
+    return makeROW(*args)
+
 def makeROW(item, name):
-    
-    global main_row
+    global queue 
     global excel_file_name
     global df_xml
     try:
-        
-        #print('makeROW')
         #csv index
         dfcols_eng = list(column.typeList[name].keys())
         dfcols = list(column.typeList[name].values())
@@ -53,50 +53,49 @@ def makeROW(item, name):
             row_df_xml = pd.DataFrame([[item.find(api_column).text for api_column in dfcols_eng]],columns=dfcols)
             pd.concat([df_xml,row_df_xml])
         else:
-            print('else')
             row_df_xml = pd.DataFrame([[item.find(api_column).text for api_column in dfcols_eng]],columns=dfcols)
             pd.concat([df_xml,row_df_xml])
 
             
-    except:
+    except Exception as e:
+        print(e)
         raise ValueError('FAIL MULTI PROCESSING') 
-    main_row+=1
 
     
 def getData():
     #가지고 있는 url만큼만 loop
     global error_count 
     error_log = open('./err.txt',mode='a')
-    while not queue.empty():
-        #저장되어있는 link를 queue에서 가져옴
-        #pool의 worker들이 link로 request 동기보다 n배 빠름
-            link = queue.get(timeout=0)
-            if link[0] != "":
-                getdata = requests.get(link[0])
-                soup = BS(getdata.text,'lxml-xml') 
-                #validation check
-                okflag = soup.find('resultCode')
-                try:
-                    if okflag.text != '00':
-                        print("okflag: ",okflag.text)
-                        
-                        raise ValueError('okcode is not 00')    
-                    else:
-                        #검색잘되면 엑셀 파싱
-                        #pool map method vs pool map_async
-                        #어떤것이 더 효율이 좋을지 결정필요
-                        item_list = soup.find_all('item')
-                        pool_excel.map([makeROW(item, link[1]) for item in item_list])
-                        print('go row')
-                except:
-                    error_log.write(link[0]+'\n')
-                    error_log.write('==================================\n')
-                    error_count+=1
-                    error_log.write(str(error_count)+'\n')
-                    queue.put(link)
+    #저장되어있는 link를 queue에서 가져옴
+    #pool의 worker들이 link로 request 동기보다 n배 빠름
+    link = queue.get(timeout=0)
+    if link[0] != "":
+        gevent.sleep(0.5)
+        getdata = requests.get(link[0])
+        soup = BS(getdata.text,'lxml-xml') 
+        #validation check
+        okflag = soup.find('resultCode')
+        try:
+            if okflag.text != '00':
+                print("okflag: ",okflag.text)
+                
+                raise ValueError('okcode is not 00')    
+            else:
+                #검색잘되면 엑셀 파싱
+                #pool map method vs pool map_async
+                #어떤것이 더 효율이 좋을지 결정필요
+                item_list = soup.find_all('item')
+                pool_excel.map(multi_wrapper,[(item, link[1]) for item in item_list])
+        except Exception as e:
+            error_log.write(link[0]+'\n')
+            error_log.write('==================================\n')
+            error_count+=1
+            error_log.write(str(error_count)+'\n')
+            print("request throw error :", e)
+            queue.put(link)
 
     print('stop crwaling')
-    print(main_row)
+    print('total error ', error_count)
     error_log.close()
 
 def init():
@@ -110,6 +109,7 @@ def init():
     while not queue.empty() :
         gevent.sleep(0.5)
         for x in range(0, min(queue.qsize(), pool.free_count())):
+            print('worker is going to work')
             pool.spawn(getData)
 
     #wait for everything complete
