b92ca9b637844a20b1d5218cfc0bb53ba04e6fdb
Q00 <jqyu.lee@gmail.com>
2019-01-03 18:06:17 +0900

    change TDD

--- apis/test.py
+++ apis/test.py
@@ -14,14 +14,14 @@ import column
 def getLink():
 
     baseUrl = 'http://apis.data.go.kr/1470000/DURPrdlstInfoService/'
-    urlList = 'getDurPrdlstInfoList',
+    urlList = 'getDurPrdlstInfoList','getSeobangjeongPartitnAtentInfoList','getEfcyDplctInfoList'
 
     for addUrl in urlList:
         for letter in range(ord('a'), ord('b')+1):
             #key는 config.py 있는 변수, async_data_crwaler에서 가져옴, key와 numofrows는 필수
-            params = {'itemname': chr(letter), 'servicekey': main.key, 'numOfRows':10}
+            params = {'itemName': chr(letter), 'servicekey': main.key, 'numOfRows':10}
             if addUrl != 'getdurprdlstinfolist':
-                params.update({'typename' : column.typeName[addUrl]}) 
+                params.update({'typeName' : column.typeName[addUrl]}) 
             #Dictionary data -> url get string으로 바꿔줌
             params_str = "&".join("%s=%s" % (k,v) for k,v in params.items()) 
             requestUrl = baseUrl + addUrl
@@ -44,24 +44,22 @@ def getLink():
                 except:
                     print('검색결과없음')
                     totalcount=0
-                #print('총 개수: ',totalcount)
+                print('총 개수: ',totalcount)
                 if totalcount!=0:
+                    flist=[]
+                    flist.append(addUrl)
+                    furl = []
                     for i in range(2):
                         print(i)
                         params_str2 = params_str
                         #request 파라미터에 pageno 추가
-                        params_str2+= '&pageno='+str(i+1)
-                        
-                        flist = []
-                        furl = requestUrl+'?'+params_str2
-                        flist.append(furl)
-                        if chr(letter) == 'b':
-                            flist.append(addUrl+'123123')
-                        else:
-                            flist.append(addUrl)
-                        #queue 에 저장
-                        main.queue.put(flist)
+                        params_str2+= '&pageNo='+str(i+1)
+                       
+                        furl.append(requestUrl+'?'+params_str2)
      
+                    flist.append(furl)
+                    #queue 에 저장
+                    main.queue.put(flist)
             except:
                 print('crwaling error : ',sys.exc_info()[1])
 #queue init
